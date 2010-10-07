9025fdfd1173e4aef5aa7b8b9a0993861fd726d6
ptx <ptx@openengine.dk>
2010-10-07 15:01:27 +0000

    First import

---
+++ Display/WallCanvas.h
@@ -0,0 +1,22 @@
+// 
+// -------------------------------------------------------------------
+// Copyright (C) 2007 OpenEngine.dk (See AUTHORS)
+//
+// This program is free software; It is covered by the GNU General
+// Public License version 2 or any later version.
+// See the GNU General Public License for more details (see LICENSE).
+//--------------------------------------------------------------------
+
+
+#ifndef _OE_WALL_CANVAS_H_
+#define _OE_WALL_CANVAS_H_
+
+
+namespace OpenEngine {
+namespace Display {
+
+
+} // NS Display
+} // NS OpenEngine
+
+#endif // _OE_WALL_CANVAS_H_
